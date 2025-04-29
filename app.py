f485355045f0ca88ee548f0e22561b38195ff9dd
Siddharth <siddharthjena412@gmail.com>
2025-04-29 13:04:08 -0400

    Fixed qiskit aer-2

--- app.py
+++ app.py
@@ -15,9 +15,8 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Conv1D, MaxPooling1D, Flatten
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
-#from qiskit.providers.aer import Aer
 from qiskit_aer import Aer
-from qiskit import execute
+from qiskit.providers.aer import execute
 from qiskit.visualization import plot_histogram
 from qiskit.circuit.library import QFT
 import requests
